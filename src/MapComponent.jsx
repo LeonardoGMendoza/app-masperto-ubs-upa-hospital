759a7ddcf9b78a2521f909cc0f5659fb01422fa2
LeonardoGMendoza <desenvolvimento3000@outlook.com>
2026-07-31 16:24:40 -0300

    fix: restaura os mapas originais do Waze

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -10,9 +10,9 @@ const MAP_STYLE = {
     'waze-tiles': {
       type: 'raster',
       tiles: [
-        'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        'https://worldtiles1.waze.com/tiles/{z}/{x}/{y}.png',
+        'https://worldtiles2.waze.com/tiles/{z}/{x}/{y}.png',
+        'https://worldtiles3.waze.com/tiles/{z}/{x}/{y}.png'
       ],
       tileSize: 256,
       attribution: '© Waze'
