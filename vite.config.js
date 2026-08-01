08271e17e0e8ca5d8ad48133c8f036f4d1bb05c3
LeonardoGMendoza <desenvolvimento3000@outlook.com>
2026-07-31 21:41:59 -0300

    build: target safari13/ios13 to fix SyntaxError while preserving classes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/app-masperto-ubs-upa-hospital/', // required for GitHub Pages
+  build: {
+    target: ['es2020', 'safari13', 'ios13']
+  }
 })
